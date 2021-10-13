1a39febfd47993014fffe8c8bc1d1c3d8e144028
Tina <z5183810@unsw.edu.au>
2021-10-14 09:51:39 +1100

    committing

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-show="showAddTask">
+        <AddTask @add-task="addTask"/>
+    </div>
+    <Tasks 
+        @toggle-reminder="toggleReminder"
+        @delete-task="deleteTask"
+        :tasks="tasks"
+    />
+</template>
+<script>
+import AddTask from './components/AddTask'
+import Tasks from './components/Tasks'
+export default {
+    name: 'Home',
+    components:{
+        Task,
+        AddTask
+    },
+    data: {
+        tasks:[s]
+    }
+}
+</script>
